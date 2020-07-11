125a795dff55b043657da4de2acc66b732c12131
Jorge Venegas <brangelina01@gmail.com>
2020-07-11 08:16:08 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,18 +1,10 @@
 def x 
     countdown = 10
   while countdown >= 0 
-      puts "#{x} SECOND(S)!"
+      puts "#{countdown} SECOND(S)!"
        countdown -= 1
     end
     "HAPPY NEW YEAR!"
 end 
 
-def countdown_with_sleep
-    x = 12
-  while x >= 0 
-      puts "#{x} SECOND(S)!"
-      sleep(1)
-       x -= 1
-    end
-    "HAPPY NEW YEAR!"
-end 
+x 
